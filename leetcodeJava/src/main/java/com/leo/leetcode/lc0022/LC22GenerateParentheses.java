06ccd607c76969678df5a49cf814637a183a2723
chenkeli <li.chenk@husky.neu.edu>
2019-06-16 11:07:14 -0400

    LC22 Generate Parentheses

---
+++ leetcodeJava/src/main/java/com/leo/leetcode/lc0022/LC22GenerateParentheses.java
@@ -0,0 +1,27 @@
+package com.leo.leetcode.lc0022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC22GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if (n == 0)
+            return res;
+        helper(res, "", n, n);
+        return res;
+    }
+
+    private void helper(List<String> res, String s, int left, int right) {
+        if (left > right)
+            return;
+        if (left == 0 && right == 0) {
+            res.add(s);
+            return;
+        }
+        if (left > 0)
+            helper(res, s + "(", left - 1, right);
+        if (right > 0)
+            helper(res, s + ")", left,right - 1);
+    }
+}
